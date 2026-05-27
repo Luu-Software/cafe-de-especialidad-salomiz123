190ab8b175e5a0b007f20dd1786805f6954f6eb6
salomiz123 <salomizrahi85@gmail.com>
2026-05-27 15:37:37 -0300

    trabajo terminado

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,17 @@ let totalTexto: string = preguntar('Total de la cuenta: ');
 let esEstudiante: string = preguntar('¿Son estudiantes de ORT? si/no: ');
 let personasTexto: string = preguntar('Dividir la cuenta entre: ');
 
-let totalPorPersona: number = 0; // COMPLETAR
+let totalPorPersona: number = 0;
+let totaldelacuenta: number = Number(totalTexto);
+let dividirlacuentaentre: number = Number(personasTexto);
+
+if(esEstudiante ==="si") {
+    totalPorPersona = totaldelacuenta * 0.9 / dividirlacuentaentre
+}
+    else {
+        totalPorPersona = totaldelacuenta / dividirlacuentaentre
+    }
+
+
 console.log(`Total por persona: $${totalPorPersona}`);
+    
